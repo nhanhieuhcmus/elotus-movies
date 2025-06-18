471a177c8b4b54ca7992c8b2abff8ae28e025b1e
nhanhieuhcmus <vosihieunhan@gmail.com>
2025-06-18 08:57:38 +0700

    add default image placeholder

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { Movie } from "../../types/Movie";
 import { Link } from "react-router-dom";
+import NoThumbPlaceholder from '../../assets/No-Image-Placeholder.svg';
 
 import "./MovieCard.scss";
 
@@ -8,12 +9,12 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const path = movie.poster_path || movie.backdrop_path;
   const bg = `https://image.tmdb.org/t/p/w342${path}`;
-
+  
   return (
     <Link to={`/movie/${movie.id}`}>
       <div className="movie-card">
         <img
-            src={bg}
+            src={path ? bg: NoThumbPlaceholder}
             alt={movie.title}
             className={`movie-img ${isLoaded ? "loaded" : ""}`}
             onLoad={() => setIsLoaded(true)}
